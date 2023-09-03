07bbe2156cff7d03c95822aada337a062f09349e
JosephGlz <joseph220199@gmail.com>
2023-09-03 16:58:41 -0600

    feat: Controller updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.use(require('./controller/GpsController'))
+router.use(require('./controller/Prompts'))
 
 module.exports = router
