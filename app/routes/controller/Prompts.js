d68537336d74cdfb6f586bb680e067aefec6da6e
JosephGlz <joseph220199@gmail.com>
2023-09-16 13:01:49 -0600

    fix: status code response fix

--- app/routes/controller/Prompts.js
+++ app/routes/controller/Prompts.js
@@ -7,14 +7,14 @@ router.use(express.json());
 router.post('/api/prompt', async (req, res) => {
     try {
         if (Object.keys(req.body).length === 0) {
-            res.status(400).json({
+            res.status(200).json({
                 status: 400,
                 message: 'Bad Request',
                 error: 'Body is required'
             });
         } else {
             if (!req.body.id || !req.body.name || !req.body.subject || !req.body.topic || !req.body.question) {
-                res.status(400).json({
+                res.status(200).json({
                     status: 400,
                     message: 'Bad Request',
                     error: 'Some required fields are missing'
