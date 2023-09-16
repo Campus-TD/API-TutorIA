e124fbaa6cde27dc4642d385418816ade2433ced
JosephGlz <joseph220199@gmail.com>
2023-09-16 13:00:54 -0600

    feat: API response fixed

--- app/routes/controller/Prompts.js
+++ app/routes/controller/Prompts.js
@@ -1,74 +1,43 @@
 const express = require('express')
 const router = express.Router()
-const { OpenAIService } = require('../../services/openai')
+const { ChatBotIA } = require('../../services/chatbotService')
 
 router.use(express.json());
 
 router.post('/api/prompt', async (req, res) => {
     try {
-        /**
-         * Body required
-         *  id: int(10) | string(11),
-         *  name: string,
-         *  subject: string,
-         *  topic: string,
-         *  question: string,
-         */
-        if(Object.keys(req.body).length === 0) {
+        if (Object.keys(req.body).length === 0) {
             res.status(400).json({
                 status: 400,
                 message: 'Bad Request',
                 error: 'Body is required'
             });
         } else {
-            if(!req.body.id) {
+            if (!req.body.id || !req.body.name || !req.body.subject || !req.body.topic || !req.body.question) {
                 res.status(400).json({
                     status: 400,
                     message: 'Bad Request',
-                    error: 'ID is required'
-                });
-            } else if(!req.body.name) {
-                res.status(400).json({
-                    status: 400,
-                    message: 'Bad Request',
-                    error: 'Name is required'
-                });
-            } else if(!req.body.subject) {
-                res.status(400).json({
-                    status: 400,
-                    message: 'Bad Request',
-                    error: 'Subject is required'
-                });
-            } else if(!req.body.topic) {
-                res.status(400).json({
-                    status: 400,
-                    message: 'Bad Request',
-                    error: 'Topic is required'
-                });
-            } else if(!req.body.question) {
-                res.status(400).json({
-                    status: 400,
-                    message: 'Bad Request',
-                    error: 'Question is required'
+                    error: 'Some required fields are missing'
                 });
             } else {
-                //TODO validate body and process
-                OpenAIService.call(req.body)
+                const response = await ChatBotIA.call(req.body);
                 res.status(200).json({
                     status: 200,
                     message: 'OK',
-                    data: req.body
+                    body: {
+                        data: response
+                    }
                 });
             }
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).json({
             status: 500,
             message: 'Internal Server Error',
-            error: error
+            error: error.message
         });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
